b64a5e97f639d0117346b91256fcd95ad9a8ec08
Amar Panchal <91047824+Amar-Panchal@users.noreply.github.com>
2022-11-01 11:32:50 +0530

    Update settings.py

--- youtube_downloader/settings.py
+++ youtube_downloader/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#03eeh(3whptvz3t^djj#huec&8q#$dr-=2z+6aj!82+y$op^'
+SECRET_KEY = 'abc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
